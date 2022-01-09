3b4809fb79fe0d3f5848ce6d647865bf568feaef
Max16 <max16@naver.com>
2022-01-10 08:23:44 +0900

    3.4 Prisma Setup

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hi",
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2022 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      return true;
+    },
   },
 };
 
