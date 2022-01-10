617b9bc12ac5f10db281dceedcc2eb420a765536
Max16 <max16@naver.com>
2022-01-10 17:28:13 +0900

    4.3 seeProfile

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Instagram Clone
 
 ## Pharmstaclone Backend
+
+### User:
+
+- [x] Create Account
+- [x] See Profile
+- [ ] Login
+- [ ] Edit Profile
+- [ ] Follow User
+- [ ] Unfollow User
+- [ ] Change Avatar (Image Upload)
---
+++ users/users.mutations.js
@@ -0,0 +1,41 @@
+import bcrypt from "bcrypt";
+import client from "../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("이미 존재하는 이메일 또는 사용자명 입니다");
+        }
+        const hashedPaswrod = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: hashedPaswrod,
+          },
+        });
+      } catch (error) {
+        return error;
+      }
+    },
+  },
+};
