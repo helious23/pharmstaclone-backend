e06c296d25d8425f8914d076dfc3a00e1880275a
Max16 <max16@naver.com>
2022-01-11 08:31:59 +0900

    4.7 editProfile

--- users/editProfile/editProfile.typeDefs.js
+++ users/editProfile/editProfile.typeDefs.js
@@ -1,6 +1,10 @@
 import { gql } from "apollo-server";
 
 export default gql`
+  type EditProfileResult {
+    ok: Boolean!
+    error: String
+  }
   type Mutation {
     editProfile(
       firstName: String
--- users/seeProfile/seeProfile.typeDefs.js
+++ users/seeProfile/seeProfile.typeDefs.js
@@ -1,11 +1,6 @@
 import { gql } from "apollo-server";
 
 export default gql`
-  type EditProfileResult {
-    ok: Boolean!
-    error: String
-  }
-
   type Query {
     seeProfile(username: String!): User
   }
